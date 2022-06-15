d31aee02b88dcc76db63fb0da7958ba3a13717b5
mach1el <th3phantom.m1@gmail.com>
2022-06-15 17:29:43 +0700

    Module to handle API

---
+++ gsmAutomation/gsmAPI.py
@@ -0,0 +1,66 @@
+import re,sys,json,requests
+from threading import *
+from requests.auth import HTTPDigestAuth
+from requests.auth import HTTPBasicAuth
+
+sys.dont_write_bytecode=True
+
+class query_sms_deliver_status(Thread):
+	def __init__(self,hostQueue,dataQueue,date):
+		Thread.__init__(self)
+		self.hostQueue = hostQueue
+		self.dataQueue = dataQueue
+		self.date = date
+		self.api = ""
+		self.session = requests.Session()
+		self.username = ""
+		self.password = ""
+		self.headers = {"Content-Type" : "application/json"}
+		self.data = {
+			"time_after":self.date,
+			"number":["1414"],
+			"port":[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
+		}
+
+	@staticmethod
+	def api_url(host):
+		return "http://"+host+"/api/query_sms_deliver_status"
+
+	@staticmethod
+	def parse_data(host,data,listPorts):
+		ports = {"host":host,"active_ports":[],"inactive_ports":[],"total_active":[]}
+		data = json.loads(data.text)['result']
+		if len(data) == 0:
+			ports["active_ports"].append("No ports")
+			ports["inactive_ports"]+=listPorts
+			ports["total_active"].append(0)
+		else:
+			for port in data:
+				if port['port'] not in ports["active_ports"]:
+					ports["active_ports"].append(port['port'])
+
+			ports["inactive_ports"]+=list(set(listPorts)-set(ports["active_ports"]))
+			ports["active_ports"].sort()
+			ports["total_active"].append(len(ports["active_ports"]))
+		return ports
+
+	def handle_request(self):
+		try:
+			resp = self.session.post(self.api,json=self.data,headers=self.headers,auth=HTTPDigestAuth(self.username,self.password),verify=False)
+		except:
+			resp = "Failed"
+		return resp
+
+	def run(self):
+		host,self.username,self.password = [element.split('=')[1] for element in self.hostQueue.get().split(" ")]
+		self.hostQueue.task_done()
+		self.api += self.api_url(host)
+		listPorts = self.data["port"]
+		resp = self.handle_request()
+		if resp == "Failed":
+			self.dataQueue.put(resp)
+			self.dataQueue.task_done()
+		else:
+			data = self.parse_data(host,resp,listPorts)
+			self.dataQueue.put(data)
+			self.dataQueue.task_done()
