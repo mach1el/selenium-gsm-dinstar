5716dcffed9e290653c167c47de47e12204f4511
mach1el <th3phantom.m1@gmail.com>
2022-06-15 17:27:11 +0700

    Manage sites ip by using yaml

---
+++ gsmAutomation/sites/__init__.py
@@ -0,0 +1,13 @@
+import os
+import sys
+import yaml
+from pathlib import Path
+
+sys.dont_write_bytecode=True
+
+dir=Path(__file__).resolve().parent
+file=os.path.join(dir,"sites.yaml")
+
+def getSite(site):
+	sites = open(file)
+	return yaml.safe_load(sites)[site]['hosts']
