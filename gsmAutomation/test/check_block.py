ce189d0a1b760bf95ca5dd42b88d31bc32d428e3
mach1el <th3phantom.m1@gmail.com>
2022-06-23 13:43:19 +0700

    made test scripts

---
+++ gsmAutomation/test/check_block.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import sys
+from pathlib import Path
+dir=Path(__file__).resolve().parent
+sys.path.append('../..')
+
+import time
+from gsmAutomation import MainArguments
+from gsmAutomation import UpdateDatabase
+from gsmAutomation.sites import getSite
+from gsmAutomation.Operator import loginSession,sendSMSphase,disablePortPhase,API
+
+site = getSite(MainArguments())
+session = loginSession(site=site)
+session.login()
+sms = sendSMSphase()
+sms.send()
+time.sleep(42)
+smsData = API(site)._check_deliver_status()
+db_session = UpdateDatabase(smsData)
+db_session._add_new_data()
+disablePort = disablePortPhase(smsData)
+disablePort.disable()
